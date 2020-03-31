82970e7886195fc2fdc6cbfd3d4a997632729a72
holle-wn <2485480043@qq.com>
2020-03-31 09:46:56 +0800

    ..

--- stu_management/stu_app/urls.py
+++ stu_management/stu_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import dispose_from
+from . import data
+
 urlpatterns = [
-path('/index',views.index)
+    path('/index', views.index)
 ]
--- stu_management/stu_app/views.py
+++ stu_management/stu_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
 
