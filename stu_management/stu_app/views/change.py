4f4280ff43bc75bc09601a1cc72b58d2950fe711
lizhenruhaha <2134933571@qq.com>
2020-04-02 17:45:19 +0800

    update页面更改and逻辑写了一点,login页面修改和逻辑 main页面

--- stu_management/stu_app/views/change.py
+++ stu_management/stu_app/views/change.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, HttpResponse
 from django.http import request
 
 
-def pwd(request):
-    pass
-
+def pwd(request,name):
+    if request.method=='GET':
+        return render(request,'../templates/update/change_pwd.html')
+    else:
+        password1=request.POST.get('pwd1')
+        password2=request.POST.get('pwd3')
+        stu=models.Student.objects.get(name=name,password=password1)
+        if stu:
+            obj=models.Student.objects.get(name=name)
+            obj.password=password2
+            obj.save()
+            return HttpResponseRedirect('/v1.0/login')
 
 def info(request):
     pass
