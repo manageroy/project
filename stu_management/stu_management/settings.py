86046293f662bd7c28cd7f4bf135a3f29ff1545b
lizhenruhaha <2134933571@qq.com>
2020-04-07 11:01:29 +0800

    template update

--- stu_management/stu_management/settings.py
+++ stu_management/stu_management/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'stu_management',
         'USER': 'root',
-        'PASSWORD': '2001724',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
