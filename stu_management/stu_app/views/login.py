c4baaddc89cc04234048b416a83a648f5130c831
lizhenruhaha <2134933571@qq.com>
2020-04-13 07:18:40 +0800

    学生信息界面,登录try

--- stu_management/stu_app/views/login.py
+++ stu_management/stu_app/views/login.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseRedirect
 from stu_app import models
 from django.core.cache import cache
 
-
 def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
@@ -11,23 +10,25 @@ def login(request):
         job_num = request.POST.get('job_num')
         password = request.POST.get('password')
         if 's' in job_num:
-            obj_stu = models.Student.objects.get(stu_num=job_num, password=password)
-            if obj_stu:
-                cache.set('stu_num', obj_stu.stu_num)
-                cache.set('name', obj_stu.name)
-                cache.set('permission', obj_stu.permission)
-                return redirect('/v1.0/main')
-            return HttpResponseRedirect('/v1.0/login')
-        else:
-            obj = models.Administrator.objects.get(job_num=job_num, password=password)
-            if obj:
-                cache.set('stu_num', obj.job_num)
-                cache.set('name', obj.name)
-                cache.set('permission', obj.permission)
-                return redirect('/v1.0/main')
-            else:
+            try:
+                obj_stu = models.Student.objects.get(stu_num=job_num, password=password)
+                if obj_stu:
+                    cache.set('stu_num', obj_stu.stu_num)
+                    cache.set('name', obj_stu.name)
+                    cache.set('permission', obj_stu.permission)
+                    return redirect('/v1.0/main')
+            except Exception as e:
                 return HttpResponseRedirect('/v1.0/login')
-
+        else:
+            try:
+                obj = models.Administrator.objects.get(job_num=job_num, password=password)
+                if obj:
+                    cache.set('stu_num', obj.job_num)
+                    cache.set('name', obj.name)
+                    cache.set('permission', obj.permission)
+                    return redirect('/v1.0/main')
+            except Exception as e:
+                return HttpResponseRedirect('/v1.0/login')
 
 def main(request):
     if cache.get('stu_num') == None:
@@ -40,7 +41,6 @@ def main(request):
         return render(request, '../templates/main/teach_main.html', {'name': cache.get('name')})
     else:
         return render(request, '../templates/main/student_main.html', {'name': cache.get('name')})
-
 
 def logout(request):
     return HttpResponseRedirect('/v1.0/login')
